b1c48d876d442b91c9093f7ca6701c27c5cc0f4d
farhad06 <farhadahamed06@gmail.com>
2026-06-08 23:38:11 +0530

    news blog home page inprogress....

--- News-Blog-CMS/src/controllers/siteController.js
+++ News-Blog-CMS/src/controllers/siteController.js
@@ -1,6 +1,22 @@
+const Category = require('../models/Category.js');
+const News = require('../models/News.js');
+const paginate = require('../utils/paginate.js');
+
 const siteController = {
     index: async (req, res) => {
-        res.render('index');
+        const paginatedNews = await paginate(News, {}, req.query, {
+            populate: [
+                { path: 'category', select: 'name slug' },
+                { path: 'author', select: 'fullName' }
+            ],
+            sort: '-createdAt'
+        })
+
+        const categoryInUse = await News.distinct('category')
+        const categories = await Category.find({ '_id': { $in: categoryInUse } })
+
+        //res.json(categories);
+        res.render('index', { paginatedNews, categories, query: req.query });
     },
     articleByCategories: async (req, res) => {
 
---
+++ News-Blog-CMS/src/utils/paginate.js
@@ -0,0 +1,32 @@
+const paginate = async (model, query = {}, reqQuery = {}, options = {}) => {
+    const { page = 1, limit = 5, sort = '-createdAt' } = reqQuery;
+
+    const paginationOptions = {
+        page: parseInt(page),
+        limit: parseInt(limit),
+        sort,
+        ...options
+    }
+
+    try {
+
+        const result = await model.paginate(query, paginationOptions);
+
+        return {
+            data: result.docs,
+            prevPage: result.prevPage,
+            nextPage: result.nextPage,
+            hasNextPage: result.hasNextPage,
+            hasPrevPage: result.hasPrevPage,
+            currentPage: result.page,
+            counter: result.pagingCounter,
+            limit: result.limit,
+            totalDocs: result.totalDocs,
+            totalPages: result.totalPages
+        }
+    } catch (err) {
+        console.error('Pagination Error ', err.message)
+    }
+}
+
+module.exports = paginate;
